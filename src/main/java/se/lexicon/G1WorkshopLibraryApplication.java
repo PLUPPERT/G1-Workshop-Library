6e6d8fda64d502e63e176cd1ea5bb869551e067a
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2024-02-14 09:45:44 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/G1WorkshopLibraryApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1WorkshopLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G1WorkshopLibraryApplication.class, args);
+	}
+
+}
